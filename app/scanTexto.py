4688a9ec3ea45af199b8bad0855a339ef1a89297
FranchiquiJ <167950323+FranchiquiJ@users.noreply.github.com>
2024-05-11 08:23:45 +0200

    g 	modified:   app/scanTexto.py

--- app/scanTexto.py
+++ app/scanTexto.py
@@ -1,32 +1,28 @@
-import os
 import cv2
 import pytesseract
 
-def scanTexto_func(imageName):
+def scanTexto_func(imagePath):
     """
     Extrae texto de una imagen y lo devuelve como cadena.
 
     Argumentos:
-        imageName (str): Nombre del archivo de imagen a escanear.
+        imagePath (str): Ruta al archivo de imagen a escanear.
 
     Retorno:
         str: El texto extraído de la imagen.
     """
 
-    # Obtener la ruta completa de la imagen
-    imagePath = os.path.join("D:/apiDocker/data/", imageName)
-
     # Cargar la imagen
     img = cv2.imread(imagePath)
 
-    # Comprobar si la imagen se leyó correctamente
+    # Comprobar si la imagen se leyó correctamente.
     if img is None:
         raise Exception("Error loading image: {}".format(imagePath))
 
     # Convertir la imagen a escala de grises
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Aplicar umbral para convertir a una imagen binaria
+    # Aplicar umbrales para convertir a una imagen binaria
     threshold_img = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
     # Establecer la ruta de Tesseract
@@ -38,9 +34,9 @@ def scanTexto_func(imageName):
     # Devolver el texto extraído
     return text
 
-# Ejemplo de uso con el nombre del archivo
-image_name = "texto.jpg"
+# Ejemplo de uso con parámetros
+image_path = "D:/apiDocker/data/texto.jpg"
 
-extracted_text = scanTexto_func(image_name)
+extracted_text = scanTexto_func(image_path)
 
 print(extracted_text)
